88ac3a9ee6b9fd44bfa2d27693fdda90ab2123a6
benmorey <dbmoreyjr@gmail.com>
2024-02-23 12:07:26 -0700

    start

--- Mission6 - David Morey/Models/Collection.cs
+++ Mission6 - David Morey/Models/Collection.cs
@@ -7,15 +7,16 @@ namespace Mission6___David_Morey.Models
     {
         [Key]
         [Required]
-        public int ID { get; set; }
-        public string Category { get; set; }
+        public int MovieID { get; set; }
+        public int CategoryID { get; set; }
         public string Title { get; set; }
         public string Year { get; set; }
         public string Director { get; set; }
         public string Rating { get; set; }
         //Allows the last three rows to be NULL
         public string? Edited { get; set; }
-        public string? Lent { get; set; }
+        public string? LentTo { get; set; }
+        public int CopiedToPlex { get; set; }
         public string? Notes { get; set; }
     }
 }
